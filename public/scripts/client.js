cd470fc8efa12e2328bf1d573181ab8bb4b0d2a6
GurpreetRandhawa <singhpreet01991@gmail.com>
2023-03-29 19:02:45 -0700

    Add cross-site scripting fix

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,7 +10,7 @@ $(document).ready(() => {
           <p>${tweet_obj.user.handle}</p>
         </header>
         <h4>
-          ${tweet_obj.content.text}
+          ${escape(tweet_obj.content.text)}
         </h4>
         <footer>
           <p class="footer-left">${timeago.format(tweet_obj.created_at)}</p>
@@ -23,18 +23,35 @@ $(document).ready(() => {
       </article>`);
   };
   const renderTweets = function (user_data) {
+    $("#tweets-container").empty();
     user_data.map((user) => {
       $("#tweets-container").append(createTweetElement(user));
     });
   };
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
   $("#tweet-form").submit((event) => {
     event.preventDefault();
-    $.ajax({
-      type: "POST",
-      url: "/tweets",
-      data: $("#tweet-form").serialize(),
-    });
+
+    if ($("#tweet-text").val() === "" || $("#tweet-text").val() === null) {
+      window.alert("The tweet is empty.");
+    } else if ($("#tweet-text").val().length > 140) {
+      window.alert("Tweet length is more than 140");
+    } else {
+      $.ajax({
+        type: "POST",
+        url: "/tweets",
+        data: $("#tweet-form").serialize(),
+        success: () => {
+          $("#tweet-form")[0].reset();
+          loadTweets();
+        },
+      });
+    }
   });
   const loadTweets = function () {
     $.ajax({
